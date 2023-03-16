f6a8a6b3201af25e9fca0504b8878b0a27c3ea80
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2023-03-16 08:33:18 -0700

    continuing work

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import gi
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+from gi.repository import Gtk, Adw
+
+_WINDOW_FILE = os.path.dirname(os.path.abspath(__file__)) + "/welcome.xml"
+_PACKAGE_FILE = os.path.dirname(os.path.abspath(__file__)) + "/package.xml"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Application(Adw.Application):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, application_id='io.risi.welcome', **kwargs)
+
+        self.builder = Gtk.Builder()
+        handlers = {
+            "on_welcomeButton_clicked": self.on_welcomeButton_clicked,
+        }
+        self.builder.connect_signals(handlers)
+        self.builder.add_from_file(_WINDOW_FILE)
+        self.window = self.builder.get_object("main_window")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def do_activate(self):
+        self.window.set_application(self)
+        self.window.present()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+    def on_welcomeButton_clicked(self, button):
+        stack = self.builder.get_object("mainStack")
+        stack.set_visible_child(self.builder.get_object("internetBox"))
+        self.window.set_application(self)
+        self.window.present()
+
+if __name__ == "__main__":
+    app = Application()
+    app.run(None)
